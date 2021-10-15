d9d60eeeab3ca8b1542ca8721984dbbab7d3dacc
onting <hemool123@naver.com>
2021-10-16 07:11:16 +0900

    Fix indent

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -35,7 +35,7 @@ def register():
             else:
                 return redirect(url_for("auth.login"))
 
-            flash(error)
+        flash(error)
 
     return render_template('auth/register.html')
 
